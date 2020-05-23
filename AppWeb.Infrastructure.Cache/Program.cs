a4511c27773e8eae9b306f95341198b1b3afab94
Felipe Santaniello <felipe.santaniello-ext@viavarejo.com.br>
2020-05-23 17:48:43 -0300

    Commit inicial

---
+++ AppWeb.Infrastructure.Cache/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppWeb.Infrastructure.Cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AppWeb.Infrastructure.SqlClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppWeb.Infrastructure.SqlClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
